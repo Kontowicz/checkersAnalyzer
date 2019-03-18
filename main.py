06f509ebbce22121450c04f01f6c028f76775c0c
Piotr Kontowicz <przemyslowiec@gmail.com>
2019-03-18 22:03:18 +0100

    Refactor: add enum

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ if __name__ == '__main__':
     while (cap.isOpened() and a!=ord('q')):
         analyzer.detectCircle()
         analyzer.drawTextInImageText()
-        cv2.imshow('Plansza', analyzer.drawBoard()[0])
-        cv2.imshow('Zaznaczone pionki', analyzer.drawBoard()[1])
-        cv2.imshow('Wizualizacja', analyzer.drawBoard()[2])
+        board = analyzer.drawBoard()
+        cv2.imshow('Plansza', board[0])
+        cv2.imshow('Zaznaczone pionki', board[1])
+        cv2.imshow('Wizualizacja', board[2])
         ret, frame = cap.read()
         analyzer.readVideo(frame)
         a = cv2.waitKey(1)
