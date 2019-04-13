03290b4709f51ba903ec821c36397659220ecec6
Piotr Kontowicz <przemyslowiec@gmail.com>
2019-04-13 15:17:20 +0200

    Add move validator

--- moveValidator/moveValidator.py
+++ moveValidator/moveValidator.py
@@ -1,39 +1,14 @@
 import copy
 from enum import  Enum
-
-class state(Enum):
-    white = 1
-    black = 2
-    empty = 0
+from board import state
 
 class moveValidator:
-    previousState: None
-
     def __init__(self, debug):
         self.debug = debug
         self.currentColorMove = state.white
-        file = open('dupm.txt', 'w')
-        file.close()
         self.previousState = None
         self.currentState = None
         self.data = []
-
-    def readData(self):
-        file = open('test.txt', 'r')
-        data = file.read()
-        data = data.split('\n')
-        lineTMP = []
-        table = []
-        for line in data:
-            if line == '':
-                self.data.append(table)
-                table = []
-                continue
-            lineTMP = []
-            for item in line:
-                if item != ' ':
-                    lineTMP.append(int(item))
-            table.append(lineTMP)
 
     def toogleColorMove(self, color):
         if color == state.black:
@@ -50,19 +25,38 @@ class moveValidator:
         self.currentColorMove = self.toogleColorMove(self.currentColorMove)
         position = self.getDiffPosition(diff) 
 
+        start = self.getStartPosition(position, colorMove)
+        stop = self.getStopPosition(position)
+
+        if self.debug:
+            print('Start: {} stop: {}'.format(start, stop))
+
         if len(position) == 2:            
-            a0 = position[0][0]
-            a1 = position[0][1]
-            a2 = position[1][0]
-            a3 = position[1][1]
-            
-            if ((self.previousState[a0][a1] == self.currentState[a2][a3]) or (self.currentState[a0][a1] == self.previousState[a2][a3])) or self.previousState[a0][a1] == colorMove.value or self.currentState[a2][a3] == colorMove.value or self.currentState[a0][a1] == colorMove.value or self.previousState[a2][a3] == colorMove.value:
-                
-                self.checkClassicMove(position)
-            else:
-                raise 'Invalid'
+            return self.checkClassicMove(colorMove, start, stop)
         if len(position) == 3:
-            self.checkBeatMove(position)
+            beat = self.getBeatPosition(position, colorMove)
+            return self.checkBeatMove(start, beat, stop, colorMove)
+
+        if self.debug:
+            print('Invalid detection')
+        else:
+            raise 'Invalid detecion'
+
+    def getStartPosition(self, position, colorMove):
+        for item in position:
+            if self.previousState[item[0]][item[1]] == colorMove.value:
+                return item
+
+    def getStopPosition(self, position):
+        for item in position:
+            if self.previousState[item[0]][item[1]] == state.empty.value:
+                return item
+
+    def getBeatPosition(self, position, colorMove):
+        beatPawnColor = self.toogleColorMove(colorMove)
+        for item in position:
+            if self.previousState[item[0]][item[1]] == beatPawnColor.value:
+                return item
 
     def readNext(self, nextState):
         if self.currentState == None:
@@ -70,14 +64,6 @@ class moveValidator:
         if self.currentState != None:
             self.previousState = copy.deepcopy(self.currentState)
             self.currentState = copy.deepcopy(nextState)
-
-    def test(self):
-        self.readData()
-        self.readNext(self.data[0])
-        diff = self.getDiff()
-        for i in range(1, len(self.data)):
-            self.readNext(self.data[i])
-            self.checkMove()
 
     def getDiff(self):
         toReturn = []
@@ -97,75 +83,134 @@ class moveValidator:
 
         return None
 
-    def checkClassicMove(self, position):
+    def checkClassicMove(self, colorMove, start, stop):
         if self.debug:
-            first = []
-            second = []
-            for item in position:
-                first.append(item[0])
-                second.append(item[1])
-
-            first.sort()
-            second.sort()
-            if first[0] + 1 == first[1] and second[0] + 1 == second[1]:
-                print('Valid move')
+            if self.previousState[start[0]][start[1]] != self.currentState[stop[0]][stop[1]]:
+                print('Invalid colors.')
+            if colorMove == state.white:
+                if stop in [[start[0] + 1, start[1] - 1], [start[0] + 1, start[1] + 1]]:
+                    print('Valid move white.')
+                else:
+                    print('Invalid move white.')
             else:
-                print('Invalid move')
+                if stop in [[start[0] - 1, start[1] - 1], [start[0] - 1, start[1] + 1]]:
+                    print('Valid move black.')
+                else:
+                    print('Invalid move black.')
         else:
-            first = []
-            second = []
-            for item in position:
-                first.append(item[0])
-                second.append(item[1])
-
-            first.sort()
-            second.sort()
-            if first[0] + 1 == first[1] and second[0] + 1 == second[1]:
-                print('Valid move')
+            if self.previousState[start[0]][start[1]] != self.currentState[stop[0]][stop[1]]:
+                raise 'Invalid pawn color.'
+            if colorMove == state.white:
+                if stop in [[start[0] + 1, start[1] - 1], [start[0] + 1, start[1] + 1]]:
+                    return True
+                else:
+                    raise 'Invalid move white.'
             else:
-                raise 'Invalid move'
+                if stop in [[start[0] - 1, start[1] - 1], [start[0] - 1, start[1] + 1]]:
+                    return True
+                else:
+                    raise 'Invalid move black.'
 
-    def checkBeatMove(self, position):
+    def checkBeatMove(self, start, beat, stop, colorMove):
         if self.debug:
-            first = []
-            second = []
-            for item in position:
-                first.append(item[0])
-                second.append(item[1])
-
-            first.sort()
-            second.sort()
-
-            if first[0] + 1 == first[1] and first[1] + 1 == first[2]:
-                if second[0] + 1 == second[1] and second[1] + 1 == second[2]:
-                    print('Valid beat')
+            if colorMove == state.white:
+                if self.previousState[start[0]][start[1]] == self.currentState[stop[0]][stop[1]]:
+                    beatPawnColor = self.toogleColorMove(colorMove)
+                    if self.previousState[beat[0]][beat[1]] == beatPawnColor.value:
+                        print('Valid beat color.')
+                    else:
+                        print('Invalid beat color.')
                 else:
-                    print('Invalid beat')
+                    print('Invalid start stop colors.')
+                
+                if beat[0] == start[0] + 1 and beat[1] == start[1] + 1:
+                    if stop[0] == beat[0] + 1 and stop[1] == beat[1] + 1:
+                        print('Valid beat white.')
+                        return True
+
+                if beat[0] == start[0] + 1 and beat[1] == start[1] - 1:
+                    if stop[0] == beat[0] + 1 and stop[1] == beat[1] - 1:
+                        print('Valid beat white.')
+                        return True
+                print('Invalid beat white.')
+                return False
+            if colorMove == state.black:
+                if self.previousState[start[0]][start[1]] == self.currentState[stop[0]][stop[1]]:
+                    beatPawnColor = self.toogleColorMove(colorMove)
+                    if self.previousState[beat[0]][beat[1]] == beatPawnColor.value:
+                        print('Valid beat color.')
+                    else:
+                        print('Invalid beat color.')
+                else:
+                    print('Invalid start stop colors.')
+                
+                if beat[0] == start[0] - 1 and beat[1] == start[1] + 1:
+                    if stop[0] == beat[0] - 1 and stop[1] == beat[1] + 1:
+                        print('Valid beat white.')
+                        return True
+
+                if beat[0] == start[0] - 1 and beat[1] == start[1] - 1:
+                    if stop[0] == beat[0] - 1 and stop[1] == beat[1] - 1:
+                        print('Valid beat white.')
+                        return True
+                print('Invalid beat white.')
+                return False
         else:
-            first = []
-            second = []
-            for item in position:
-                first.append(item[0])
-                second.append(item[1])
-
-            first.sort()
-            second.sort()
-
-            if first[0] + 1 == first[1] and first[1] + 1 == first[2]:
-                if second[0] + 1 == second[1] and second[1] + 1 == second[2]:
-                    print('Valid beat')
+            if colorMove == state.white:
+                if self.previousState[start[0]][start[1]] == self.currentState[stop[0]][stop[1]]:
+                    beatPawnColor = self.toogleColorMove(colorMove)
+                    if self.previousState[beat[0]][beat[1]] == beatPawnColor.value:
+                        print('Valid beat color.')
+                    else:
+                        raise 'Invalid beat color.'
                 else:
-                    raise 'Invalid move'
+                    raise 'Invalid start stop colors.'
+                
+                if beat[0] == start[0] + 1 and beat[1] == start[1] + 1:
+                    if stop[0] == beat[0] + 1 and stop[1] == beat[1] + 1:
+                        return True
+
+                if beat[0] == start[0] + 1 and beat[1] == start[1] - 1:
+                    if stop[0] == beat[0] + 1 and stop[1] == beat[1] - 1:
+                        return True
+
+                raise 'Invalid beat white.'
+            if colorMove == state.black:
+                if self.previousState[start[0]][start[1]] == self.currentState[stop[0]][stop[1]]:
+                    beatPawnColor = self.toogleColorMove(colorMove)
+                    if self.previousState[beat[0]][beat[1]] == beatPawnColor.value:
+                        print('Valid beat color.')
+                    else:
+                        raise 'Invalid beat color.'
+                else:
+                    raise 'Invalid start stop colors.'
+                
+                if beat[0] == start[0] - 1 and beat[1] == start[1] + 1:
+                    if stop[0] == beat[0] - 1 and stop[1] == beat[1] + 1:
+                        return True
+
+                if beat[0] == start[0] - 1 and beat[1] == start[1] - 1:
+                    if stop[0] == beat[0] - 1 and stop[1] == beat[1] - 1:
+                        return True
+
+                raise 'Invalid beat white.'
 
     def getDiffPosition(self, diff):
         position = []
-
         for i in range(0, 8):
             for j in range(0, 8):
                 if diff[i][j] == 1:
                     position.append([i, j])
         
         return position
+
+    # For testing
+    def test(self, fileName):
+        self.readData(fileName)
+
+        for i in range(0, len(self.data)):
+            self.readNext(self.data[i])
+            self.checkMove()
 
     def dumpToFile(self, diff):
         file = open('dump.txt', 'a')
@@ -190,6 +235,46 @@ class moveValidator:
             file.write('\n')
         file.close()
 
+    def writeToFile(self):        
+        file_current = open('currentState.txt', 'w')
+        file_prev = open('prevState.txt', 'w')
+        for i in range(0, len(self.data) - 1):
+            tmp = self.getDiffTmp(self.data[i], self.data[i+1])
+            if tmp != None:
+                print(tmp)
+                for item in self.data[i]:
+                    for elem in item:
+                        file_current.write('{} '.format(elem))
+                    file_current.write('\n')
+                file_current.write('\n')
+
+                for item in self.data[i+1]:
+                    for elem in item:
+                        file_prev.write('{} '.format(elem))
+                    file_prev.write('\n')
+                file_prev.write('\n')                
+            
+        file_current.close()
+        file_prev.close()
+
+    def readData(self, fileName):
+        file = open(fileName, 'r')
+        data = file.read()
+        data = data.split('\n')
+        lineTMP = []
+        table = []
+        for line in data:
+            if line == '':
+                self.data.append(table)
+                table = []
+                continue
+            lineTMP = []
+            for item in line:
+                if item != ' ':
+                    lineTMP.append(int(item))
+            table.append(lineTMP)
+
+
 if __name__ == "__main__":
     mv = moveValidator(True)
-    mv.test()
+    mv.test('allMoveValid.txt')
